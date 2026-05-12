726146c3f408fef891b1e5215a1477f1d29769b8
Slava <chaplyhin.viacheslav@gmail.com>
2026-05-12 19:37:07 +0300

    updates

---
+++ src/js/1-timer.js
@@ -0,0 +1,24 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+
+const input = document.querySelector("#datetime-picker");
+const startBtn = document.querySelector("[data-start]");
+const daysEl = document.querySelector("[data-days]");
+const hoursEl = document.querySelector("[data-hours]");
+const minutesEl = document.querySelector("[data-minutes]");
+const secondsEl = document.querySelector("[data-seconds]");
+let userSelectedDate  = null;
+
+startBtn.disabled = true;
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      console.log(selectedDates[0]);
+      startBtn.disabled = false;
+  },
+};
+flatpickr(input, options); 
